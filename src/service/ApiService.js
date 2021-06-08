d15b665e67074eb44232303dcc7234cd57f71211
ragimovalik <ragimovalik@gmail.com>
2021-06-09 00:12:21 +0300

    Some cleanup

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -2,6 +2,23 @@ import { API_KEY } from './ApiKey';
 
 const BASE_URL = 'https://pixabay.com/api/';
 
+const fetchOn = async (query, pageNumber, pageCapacity = 12) => {
+  const url = `${BASE_URL}/?q=${query}&page=${pageNumber}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${pageCapacity} `;
+
+  try {
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      console.log('Server response by non 200 status');
+      return;
+    } else return response.json();
+  } catch (error) {
+    return console.log('Server Error. Something gone wrong.', error);
+  }
+};
+
+export default fetchOn;
+
+/*
 const fetchOn = (query, pageNumber, pageCapacity = 12) => {
   const url = `${BASE_URL}/?q=${query}&page=${pageNumber}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${pageCapacity} `;
 
@@ -17,5 +34,4 @@ const fetchOn = (query, pageNumber, pageCapacity = 12) => {
     })
     .catch(error => console.log('Server Error. Something gone wrong.', error));
 };
-
-export default fetchOn;
+*/
